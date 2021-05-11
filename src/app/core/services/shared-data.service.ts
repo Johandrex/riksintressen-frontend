8b2c33370f66900885c98816deb6bc0947ab3d74
Seb <Sebastian.Sjoberg.4578@student.uu.se>
2021-05-11 11:28:18 +0200

    Restructured sidebar components.

--- src/app/core/services/shared-data.service.ts
+++ src/app/core/services/shared-data.service.ts
@@ -11,9 +11,14 @@ import { ApiService } from './api.service';
   providedIn: 'root'
 })
 export class SharedDataService {
+  // Maintains type of selected information sidebar
+  private sidebarSource = new BehaviorSubject<string>("");
+  public sidebarCurrent = this.sidebarSource.asObservable();
+
   // Checks whether user has selected an item from a list to display
   public hasSelectedItem : boolean = false;
 
+  // Maintains the selected national interest
   private idSource = new BehaviorSubject<number>(0);
   public currentId = this.idSource.asObservable();
   
@@ -29,6 +34,14 @@ export class SharedDataService {
   constructor(private api: ApiService) {
     this.subscribeToNationalInterestsList();
     this.subcribeToCategories();
+  }
+
+  /**
+   * Changes sidebar based on input.
+   * @param sidebar The sidebar that has been selected.
+   */
+   public changeInformationSidebarDisplayed(sidebar: string) : void {
+    this.sidebarSource.next(sidebar);
   }
 
   /**
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderButtonComponent } from './buttons/header-button/header-button.component';
-import { SidebarInfoRiksintresseComponent } from './sidebars/sidebar-info-riksintresse/sidebar-info-riksintresse.component';
-import { SidebarEditRiksintresseComponent } from './sidebars/sidebar-edit-riksintresse/sidebar-edit-riksintresse.component';
-import { SidebarHelpComponent } from './sidebars/sidebar-help/sidebar-help.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,10 +9,7 @@ import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [
-    HeaderButtonComponent,
-    SidebarInfoRiksintresseComponent,
-    SidebarEditRiksintresseComponent,
-    SidebarHelpComponent
+    HeaderButtonComponent
   ],
   imports: [
     CommonModule,
@@ -26,10 +20,7 @@ import { NgSelectModule } from '@ng-select/ng-select';
     NgSelectModule,
   ],
   exports: [
-    HeaderButtonComponent,
-    SidebarInfoRiksintresseComponent,
-    SidebarEditRiksintresseComponent,
-    SidebarHelpComponent
+    HeaderButtonComponent
   ]
 })
 export class SharedModule { }
